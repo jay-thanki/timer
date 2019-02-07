41e2b329bbbcf0f46f8519ba3a04cfefc7aeb595
jay-thanki <34687629+jay-thanki@users.noreply.github.com>
2019-02-07 17:20:54 +0530

    Update main.js
    remove api error code

--- main.js
+++ main.js
@@ -46,21 +46,21 @@ app.on('activate', function () {
   }
 })
 
-const { app } = require('electron')
-app.on('ready', () => {
-  const { net } = require('electron')
-  const request = net.request('https://github.com')
-  request.on('response', (response) => {
-    console.log(`STATUS: ${response.statusCode}`)
-    console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
-    response.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`)
-    })
-    response.on('end', () => {
-      console.log('No more data in response.')
-    })
-  })
-  request.end()
-})
+// const { app } = require('electron')
+// app.on('ready', () => {
+//   const { net } = require('electron')
+//   const request = net.request('https://github.com')
+//   request.on('response', (response) => {
+//     console.log(`STATUS: ${response.statusCode}`)
+//     console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+//     response.on('data', (chunk) => {
+//       console.log(`BODY: ${chunk}`)
+//     })
+//     response.on('end', () => {
+//       console.log('No more data in response.')
+//     })
+//   })
+//   request.end()
+// })
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
